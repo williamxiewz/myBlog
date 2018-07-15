545e7450dc6a008260305940a2a92d183fed14ac
williamxie <hyw540667209@gmail.com>
2018-07-15 17:46:17 +0800

    init project

---
+++ Apple Developer/App Architecture/App Architecture/App Architecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Architecture
+//
+//  Created by williamxie on 2018/7/15.
+//  Copyright © 2018 williamxie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
